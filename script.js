b54a9133a36d475bbd709f6317c4f316c60f2ae8
AleemS7 <aleemshaik1225@gmail.com>
2023-02-04 23:40:28 -0500

    8

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
-const btn = document.querySelector('#btn');
-const quoteEl = document.querySelector('#quote');
-const apiKey = live_WZ4717FjTnIVTn29CGC4ARXy2t0VRqlzQGYsvmgS47LJqV3PaNhKoMZQRNhTL7Rk;
+// Your API key for TheCatAPI
+const API_KEY = "live_WZ4717FjTnIVTn29CGC4ARXy2t0VRqlzQGYsvmgS47LJqV3PaNhKoMZQRNhTL7Rk";
 
-btn.addEventListener('click', () => {
-  fetch('https://api.quotable.io/random')
-    .then(response => response.json())
-    .then(data => {
-      quoteEl.innerText = data.content;
-    });
+// Function to get a random cat image
+async function getCatImage() {
+  const response = await fetch(`https://api.thecatapi.com/v1/images/search?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data[0].url;
+}
 
-  fetch(`https://thecatapi.com/api/images/get?format=src&type=gif&api_key=${apiKey}`)
-    .then(response => response.url)
-    .then(url => {
-      document.body.style.backgroundImage = `url(${url})`;
-    });
+// Function to get a random uplifting quote
+async function getQuote() {
+  const response = await fetch("https://api.quotable.io/random");
+  const data = await response.json();
+  return data.content;
+}
+
+// Button click event to change the quote and cat image
+document.querySelector("button").addEventListener("click", async function() {
+  const catImage = await getCatImage();
+  document.querySelector(".cat-image").src = catImage;
+  document.querySelector(".quote").textContent = await getQuote();
+});
+
+// Initial call on page load
+getCatImage().then(catImage => {
+  document.querySelector(".cat-image").src = catImage;
+});
+
+getQuote().then(quote => {
+  document.querySelector(".quote").textContent = quote;
 });
 
